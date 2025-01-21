4537c88d3a0552e337f52ada8fbb0f86ff1029ad
DzygunRoman <dzygun-roman@mail.ru>
2025-01-21 10:58:44 +0300

    работаю

--- siteKeysAndPhoto/keysAndPhoto/urls.py
+++ siteKeysAndPhoto/keysAndPhoto/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('cat/<int:cat_id>/', views.categories),
+    path('addfile/', views.addfile, name='add_file'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
--- siteKeysAndPhoto/keysAndPhoto/views.py
+++ siteKeysAndPhoto/keysAndPhoto/views.py
@@ -2,14 +2,21 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
-
-menu = ["О сайте", "Обратная связь", "Войти"]
+menu = [
+    {'title': "О сайте", 'url_name': 'about'},
+    {'title': "Добавить файлы", 'url_name': 'add_file'},
+    {'title': "Обратная связь", 'url_name': 'contact'},
+    {'title': "Войти", 'url_name': 'login'},
+]
 
 data_db = [
-    {'id':1, 'name':'3 на 4','content':' подходит для большинства документов', 'price':'300р', 'is_published':True},
-    {'id':2, 'name':'4 на 6','content':' удостоверение охранника, военкомат на контракт', 'price':'300р', 'is_published':False},
-    {'id':3, 'name':'3,5 на 4,5','content':' паспорт, загран-паспорт', 'price':'300р', 'is_published':True},
+    {'id': 1, 'name': '3 на 4', 'content': ' подходит для большинства документов', 'price': '300р',
+     'is_published': True},
+    {'id': 2, 'name': '4 на 6', 'content': ' удостоверение охранника, военкомат на контракт', 'price': '300р',
+     'is_published': False},
+    {'id': 3, 'name': '3,5 на 4,5', 'content': ' паспорт, загран-паспорт', 'price': '300р', 'is_published': True},
 ]
+
 
 def index(request):
     data = {
@@ -17,15 +24,27 @@ def index(request):
         'menu': menu,
         'posts': data_db,
     }
-
     return render(request, 'keysAndPhoto/index.html', context=data)
 
 
 def about(request):
     return render(request, 'keysAndPhoto/about.html', {'title': 'О сайте'})
 
-def categories(request, cat_id):
-    return HttpResponse("Информация по категориям")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Детальная информация об услуге {post_id}")
+
+
+def addfile(request):
+    return HttpResponse("Добавление файлов")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Войти")
 
 
 def page_not_found(request, exception):
